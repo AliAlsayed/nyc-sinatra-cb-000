ccd98fa887f812efcca31653ad73f9e8d36cd28a
Ali Alsayed <a_alsayed87@hotmail.com>
2019-02-25 07:19:51 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,6 +11,7 @@ class LandmarksController < ApplicationController
 
   get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
   end
 
   post '/landmarks' do
